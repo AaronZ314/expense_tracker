0dcf200c5bc54c4b9cb9986bc96592ac99d00bb7
AaronZ314 <aaronminxz@gmail.com>
2026-03-31 11:40:29 -0400

    Updated card to include amount and date

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -11,7 +11,20 @@ class ExpenseItem extends StatelessWidget{
         horizontal: 20,
         vertical: 16,
       ),
-      child: Card(child: Text(expense.title),
+      child: Column(
+        children: [
+          Text(expense.title), //Text box at the top of column
+          const SizedBox(height: 4), //A little space between first row of the column and second column
+          Row(children: [ //We need a bunch of things in this row of the column so we add a row
+            Text('\$${expense.amount.toStringAsFixed(2)}'), //Amount first
+            Spacer(), //Basically pushes everything to the right
+            Row(children: [ //Cat and date closely grouped so another row in this row
+            const Icon(Icons.question_mark), //Change this later to look up a correct icon for the cat
+            const SizedBox(width: 8),
+            Text(expense.date.toString()) //fix this date to be correclty formatted
+            ],)
+          ],)
+        ],
       ),
     );
   }
