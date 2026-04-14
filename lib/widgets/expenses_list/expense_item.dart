ad8712a55b54ac9c2e695668b7cd8f91df158593
AaronZ314 <aaronminxz@gmail.com>
2026-04-14 12:12:53 -0400

    started theming

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -6,25 +6,30 @@ class ExpenseItem extends StatelessWidget{
   final Expense expense;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 16,
-      ),
-      child: Column(
-        children: [
-          Text(expense.title), //Text box at the top of column
-          const SizedBox(height: 4), //A little space between first row of the column and second column
-          Row(children: [ //We need a bunch of things in this row of the column so we add a row
-            Text('\$${expense.amount.toStringAsFixed(2)}'), //Amount first
-            Spacer(), //Basically pushes everything to the right
-            Row(children: [ //Category and date closely grouped so another row in this row
-            Icon(categoryIcons[expense.category]),
-            const SizedBox(width: 8),
-            Text(expense.formattedDate)
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expense.title,
+            style: Theme.of(context).textTheme.titleLarge,
+            ), //Text box at the top of column
+            const SizedBox(height: 4), //A little space between first row of the column and second column
+            Row(children: [ //We need a bunch of things in this row of the column so we add a row
+              Text('\$${expense.amount.toStringAsFixed(2)}'), //Amount first
+              Spacer(), //Basically pushes everything to the right
+              Row(children: [ //Category and date closely grouped so another row in this row
+              Icon(categoryIcons[expense.category]),
+              const SizedBox(width: 8),
+              Text(expense.formattedDate)
+              ],)
             ],)
-          ],)
-        ],
+          ],
+        ),
       ),
     );
   }
