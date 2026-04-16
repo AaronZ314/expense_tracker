40b16cf36f3f65dcec3597af93426690ccabd0da
AaronZ314 <aaronminxz@gmail.com>
2026-04-16 11:50:30 -0400

    added graph

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -24,3 +24,21 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for(final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+  ExpenseBucket.forCategory(
+    List<Expense> allExpenses, this.category)
+    : expenses = allExpenses.where((expense) => expense.category == category).toList();
+}
